a7f89796b5f44a59e65651df1409c505977058e2
gt-yordanos <gt.yordanos@gmail.com>
2024-09-06 11:05:22 +0300

    craeted albums data component and mounted the display home component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {albumsData} from '../assets/assets'
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar/>
+      <div className="my-5 font-bold text-2xl">
+        
+      </div>
     </>
   )
 }
