d37efc95f4be1f5e9b29becb8ff858ce6692e7cd
gt-yordanos <gt.yordanos@gmail.com>
2024-09-06 11:49:22 +0300

    i mapped over the songs item component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import {albumsData, songsData} from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem '
 
 const DisplayHome = () => {
   return (
@@ -17,7 +18,7 @@ const DisplayHome = () => {
       <div className="mb-4">
         <h1 className='my-5 font-bold text-2xl'>Today's biggest hit</h1>
         <div className='flex overflow-auto hide-scrollbar'>
-          {songsData.map(()=>())}
+          {songsData.map((item, index)=>(<SongItem key={index}  name={item.name} image={item.image} desc={item.desc} id={item.id}/>))}
         </div>
       </div>
     </>
