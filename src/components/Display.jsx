23fa1fc9826797d4d38f4c9cd5edbc3ca2f60ee0
gt-yordanos <gt.yordanos@gmail.com>
2024-09-06 11:35:29 +0300

    created a song Item component and mounted it in to the display home component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,7 +4,7 @@ import DisplayHome from './DisplayHome'
 
 const Display = () => {
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auton lg:w-[75%]'>
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%]'>
       <Routes>
         <Route path ='/' element={<DisplayHome/>}/>
       </Routes>
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
-import {albumsData} from '../assets/assets'
+import {albumsData, songsData} from '../assets/assets'
+import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar/>
-      <div className="my-5 font-bold text-2xl">
-        
+      <div className="mb-4">
+        <h1 className='my-5 font-bold text-2xl'>Feautured Charts</h1>
+        <div className='flex overflow-auto hide-scrollbar'>
+          {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <h1 className='my-5 font-bold text-2xl'>Today's biggest hit</h1>
+        <div className='flex overflow-auto hide-scrollbar'>
+          {songsData.map(()=>())}
+        </div>
       </div>
     </>
   )
