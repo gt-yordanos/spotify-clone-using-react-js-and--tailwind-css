1fa92cb56f8529d43934e942f4c198ed66fe47d6
gt-yordanos <gt.yordanos@gmail.com>
2024-09-07 12:49:39 +0300

    added a functionality to open a music and  seekbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
